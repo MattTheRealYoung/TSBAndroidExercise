5476c1f67a8352b2a51679dbf41789e81e78d32b
mattyang <yangchengyc2008@gmail.com>
2025-03-21 16:35:12 +1300

    Implement first function: when an accounting record is selected, it is subtracted from the remaining total at the top.

--- app/src/main/java/co/nz/tsb/interview/bankrecmatchmaker/FindMatchActivity.java
+++ app/src/main/java/co/nz/tsb/interview/bankrecmatchmaker/FindMatchActivity.java
@@ -14,6 +14,9 @@ import java.util.List;
 public class FindMatchActivity extends AppCompatActivity {
 
     public static final String TARGET_MATCH_VALUE = "co.nz.tsb.interview.target_match_value";
+    // Record the remaining total number
+    private float remainingTotal;
+    private TextView matchText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +24,7 @@ public class FindMatchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_find_match);
 
         Toolbar toolbar = findViewById(R.id.toolbar);
-        TextView matchText = findViewById(R.id.match_text);
+        matchText = findViewById(R.id.match_text);
         RecyclerView recyclerView = findViewById(R.id.recycler_view);
 
         setSupportActionBar(toolbar);
@@ -31,13 +34,33 @@ public class FindMatchActivity extends AppCompatActivity {
 
         float target = getIntent().getFloatExtra(TARGET_MATCH_VALUE, 10000f);
         matchText.setText(getString(R.string.select_matches, (int) target));
+        // Initial remaining number
+        remainingTotal = target;
+        // Update match text
+        updateMatchText();
 
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         List<MatchItem> items = buildMockData();
-        final MatchAdapter adapter = new MatchAdapter(items);
+        // Register the subscribe to handle the item check event and update with the latest result
+        final MatchAdapter adapter = new MatchAdapter(items, (item, isChecked) -> {
+            if (isChecked) {
+                remainingTotal -= item.getTotal();
+            } else {
+                remainingTotal += item.getTotal();
+            }
+            // Update the UI
+            updateMatchText();
+        });
 
         recyclerView.setAdapter(adapter);
+    }
+
+    /**
+     * Update match text to reflect the latest result
+     */
+    private void updateMatchText(){
+        matchText.setText(getString(R.string.select_matches, (int) remainingTotal));
     }
 
     private List<MatchItem> buildMockData() {
--- app/src/main/java/co/nz/tsb/interview/bankrecmatchmaker/MatchAdapter.java
+++ app/src/main/java/co/nz/tsb/interview/bankrecmatchmaker/MatchAdapter.java
@@ -17,28 +17,55 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> 
         private TextView total;
         private TextView subtextLeft;
         private TextView subtextRight;
+        private CheckedListItem checkedListItem;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            checkedListItem = (CheckedListItem) itemView;
             mainText = itemView.findViewById(R.id.text_main);
             total = itemView.findViewById(R.id.text_total);
             subtextLeft = itemView.findViewById(R.id.text_sub_left);
             subtextRight = itemView.findViewById(R.id.text_sub_right);
         }
 
-        public void bind(MatchItem matchItem) {
+        /**
+         * We should pass in the item check listener and position to bind with the view, once user
+         * checked the MatchItem, subscribers will get the update
+         * @param matchItem
+         * @param listener
+         * @param position
+         */
+        public void bind(MatchItem matchItem, OnItemCheckedListener listener, int position) {
             mainText.setText(matchItem.getPaidTo());
             total.setText(Float.toString(matchItem.getTotal()));
             subtextLeft.setText(matchItem.getTransactionDate());
             subtextRight.setText(matchItem.getDocType());
+            // Handle click event to toggle checkbox
+            checkedListItem.setOnClickListener(v -> {
+                boolean isChecked = !checkedListItem.isChecked();
+                checkedListItem.setChecked(isChecked);
+                //notify subscribers
+                listener.onItemChecked(matchItem, isChecked);
+            });
         }
-
     }
 
     private List<MatchItem> matchItems;
+    /**
+     * OnItemCheckedListener, listen the check behavior when user checked the MatchItem
+     */
+    private OnItemCheckedListener listener;
 
-    public MatchAdapter(List<MatchItem> matchItems) {
+    /**
+     * provide an interface to handle onItemChecked event
+     */
+    public interface OnItemCheckedListener {
+        void onItemChecked(MatchItem item, boolean isChecked);
+    }
+
+    public MatchAdapter(List<MatchItem> matchItems, OnItemCheckedListener listener) {
         this.matchItems = matchItems;
+        this.listener = listener;
     }
 
     @Override
@@ -51,7 +78,7 @@ public class MatchAdapter extends RecyclerView.Adapter<MatchAdapter.ViewHolder> 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
         MatchItem matchItem = matchItems.get(position);
-        holder.bind(matchItem);
+        holder.bind(matchItem, listener, position);
     }
 
     @Override
